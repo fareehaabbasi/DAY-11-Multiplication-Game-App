53dce70921b816358aa2aab368126b3682e9caf1
fareehaabbasi <abbasifareeha9@gmail.com>
2025-08-25 22:05:03 +0500

    complete

--- app.js
+++ app.js
@@ -10,15 +10,18 @@ btn.addEventListener('click', () => {
     let val = input.value;
     val = val.trim();
     val = Number(val);
-    if(input.value === '') {
-        alert('Please enter a number');
-    }else if (val === num1.textContent * num2.textContent) {
+    if (input.value === '') {
+        Swal.fire({
+            icon: "error",
+            text: "You need to enter a value",
+        });
+    } else if (val === num1.textContent * num2.textContent) {
         count++;
         score.textContent = count;
         num1.textContent = Math.floor(Math.random() * 10);
         num2.textContent = Math.floor(Math.random() * 10);
         input.value = '';
-    }else {
+    } else {
         alert('Wrong answer. Try again!');
         num1.textContent = Math.floor(Math.random() * 10);
         num2.textContent = Math.floor(Math.random() * 10);
